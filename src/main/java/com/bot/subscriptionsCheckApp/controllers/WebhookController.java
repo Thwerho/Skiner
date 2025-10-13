fd6a2bf89888208bc352c84d87755ce03181d0ae
mollen <stepanpanenko@gmail.com>
2025-10-13 07:50:18 +0300

    application.yaml webhook

--- src/main/java/com/bot/subscriptionsCheckApp/controllers/WebhookController.java
+++ src/main/java/com/bot/subscriptionsCheckApp/controllers/WebhookController.java
@@ -4,18 +4,20 @@ import com.bot.subscriptionsCheckApp.Telegram.ContestJoinBot;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 @RestController
+@RequestMapping("/webhook")
 @RequiredArgsConstructor
 public class WebhookController
 {
 
     private final ContestJoinBot contestJoinBot;
 
-    @PostMapping("/webhook")
+    @PostMapping
     public BotApiMethod<?> onUpdateReceived(@RequestBody Update update)
     {
         return contestJoinBot.onWebhookUpdateReceived(update);
